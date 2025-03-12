4f018bfe7db9112fa96cd7176be2160d671f3b96
Andrew Wallen <andrew.wallen@kellogg.northwestern.edu>
2025-03-12 02:57:33 +0000

    added bootstrap information

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+  get("/login", { :controller => "sessions", :action => "new"})
+  post("/login", { :controller => "sessions", :action => "create"})
+  delete("/logout", { :controller => "sessions", :action => "destroy"})
   resources "entries"
   resources "places"
   resources "sessions"
