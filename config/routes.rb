864b7c101f6af439d282c163cdf40118c0408792
Andrew Wallen <andrew.wallen@kellogg.northwestern.edu>
2025-03-12 15:23:31 +0000

    updates through additional rubric items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
   get("/login", { :controller => "sessions", :action => "new"})
   post("/login", { :controller => "sessions", :action => "create"})
-  delete("/logout", { :controller => "sessions", :action => "destroy"})
+  get("/logout", { :controller => "sessions", :action => "destroy"})
   resources "entries"
   resources "places"
   resources "sessions"
